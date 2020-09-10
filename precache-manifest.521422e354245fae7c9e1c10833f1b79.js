096e36ef6a20fb9b710384b201d59cc7e22e888a
Tanya Kravchenko <mail-tanja@mail.ru>
2020-09-10 23:37:19 +0300

    Updates

---
+++ precache-manifest.521422e354245fae7c9e1c10833f1b79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bda538b2a3791abe19c12ce70d03615",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "b7f0167d9efe33691bcc",
+    "url": "/counter/static/css/main.401847e3.chunk.css"
+  },
+  {
+    "revision": "66fd6364fa738c1d2f17",
+    "url": "/counter/static/js/2.c5ffc7a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.c5ffc7a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f0167d9efe33691bcc",
+    "url": "/counter/static/js/main.3b277503.chunk.js"
+  },
+  {
+    "revision": "890e0606e2c9e34496c7",
+    "url": "/counter/static/js/runtime-main.01fdcc42.js"
+  }
+]);
